0fe0433c09cea8a49220f453e87d563f2775df58
lucasportella <lucasportellaagu@gmail.com>
2020-05-10 12:54:39 -0300

    ex056.0 line 20 ok, line 18 bugged

--- python-codes/curso_em_video_estruturas_de_controle/ex056.0.py
+++ python-codes/curso_em_video_estruturas_de_controle/ex056.0.py
@@ -1,7 +1,8 @@
 somaid = 0
-contm = 0
+contmm = 0
 contm2 = 0
 conth = 0
+maisvelho = 0
 
 for c in range(1,5,1):
     nome = str(input('Nome da {}ª pessoa: '.format(c))).capitalize().strip()
@@ -9,18 +10,14 @@ for c in range(1,5,1):
     sexo = str(input('Sexo da {}ª pessoa(digite H para homem e M para mulher): '.format(c))).lower().strip()
     somaid += idade
     if sexo == 'm' and idade < 20:
-        contm += 1
+        contmm += 1
     elif sexo == 'm':
         contm2 += 1
-    elif c == 1 and sexo == 'h':
-        maisvelho = idade
-        nomevelho = nome
+    elif sexo == 'h':
         conth += 1
-    elif c > 1 and sexo == 'h':
         if maisvelho < idade:
             maisvelho = idade
             nomevelho = nome
-            conth += 1
 
 media = somaid/4
 print('A média de idade do grupo é: {:.1f} anos'.format(media))
@@ -28,10 +25,12 @@ print('A média de idade do grupo é: {:.1f} anos'.format(media))
 if conth >=1:
     print('O homem mais velho se chama {}.'.format(nomevelho))
 
-elif contm == 0 and contm2 > 0:
+if contm2 == 0:
+    print('Não há nenhuma mulher no grupo.')
+elif contmm == 0 and contm2 > 0:
     print('Nenhuma mulher tem menos de 20 anos.')
-elif contm == 1:
-    print('{} mulher tem menos de 20 anos.'.format(contm))
-elif contm > 1:
-    print('{} mulheres tem menos de 20 anos.'.format(contm))
+elif contmm == 1:
+    print('{} mulher tem menos de 20 anos.'.format(contmm))
+elif contmm > 1:
+    print('{} mulheres tem menos de 20 anos.'.format(contmm))
 
