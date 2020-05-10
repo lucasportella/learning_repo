026de52ff2ac955b1cf7b56194c87253499773ac
lucasportella <lucasportellaagu@gmail.com>
2020-05-10 19:07:53 -0300

    ex056.0 bugged draw

--- python-codes/curso_em_video_estruturas_de_controle/ex056.0.py
+++ python-codes/curso_em_video_estruturas_de_controle/ex056.0.py
@@ -3,27 +3,36 @@ contmm = 0
 contm2 = 0
 conth = 0
 maisvelho = 0
+nomevelho = ''
 
 for c in range(1,5,1):
+    print(' -----{}ª PESSOA -----'.format(c))
     nome = str(input('Nome da {}ª pessoa: '.format(c))).capitalize().strip()
     idade = int(input('Idade da {}ª pessoa: '.format(c)))
-    sexo = str(input('Sexo da {}ª pessoa(digite H para homem e M para mulher): '.format(c))).lower().strip()
+    sexo = str(input('Sexo da {}ª pessoa [M/F]: '.format(c))).lower().strip()
     somaid += idade
-    if sexo == 'm' and idade < 20:
+    if sexo == 'f' and idade < 20:
         contmm += 1
-    elif sexo == 'm':
         contm2 += 1
-    elif sexo == 'h':
+    elif sexo == 'f':
+        contm2 += 1
+
+    elif sexo == 'm':
         conth += 1
-        if maisvelho < idade:
+        if idade >= maisvelho:
             maisvelho = idade
             nomevelho = nome
+        else:
+            print('Erro, idade não pode ser menor que zero.')
 
 media = somaid/4
 print('A média de idade do grupo é: {:.1f} anos'.format(media))
 
 if conth >=1:
-    print('O homem mais velho se chama {}.'.format(nomevelho))
+    print('O homem mais velho se chama {} e ele tem {} ano(s).'.format(nomevelho,maisvelho))
+elif conth == 0:
+    print('Não há nenhum homem no grupo.')
+
 
 if contm2 == 0:
     print('Não há nenhuma mulher no grupo.')
