d98052c895b44ea8fba41adebb3b13b2ac795d65
lucasportella <lucasportellaagu@gmail.com>
2020-05-19 10:23:42 -0300

    exs 65.1

--- python-codes/curso_em_video_estruturas_de_controle/ex065.1.py
+++ python-codes/curso_em_video_estruturas_de_controle/ex065.1.py
@@ -1,6 +1,5 @@
 n = s = c = mai = men = 0
 n = int(input('Digite um número [999 p/ parar]: '))
-men = n
 while n != 999:
     c += 1
     s += n
@@ -11,6 +10,7 @@ while n != 999:
     n = int(input('Digite um número [999 p/ parar]: '))
 if s == 0 and c == 0:
     m = 'indeterminado'
+    print('Soma:', s, '\nQuant. números:', c, '\nMédia:',m, '\nMaior:', mai, '\nMenor:', men)
 else:
     m = s/c
-print('Soma:',s,'\nQuant. números:',c,'\nMédia:',m,'\nMaior:',mai,'\nMenor:',men)
+    print('Soma:',s,'\nQuant. números:',c,'\nMédia: {:.1f}'.format(m),'\nMaior:',mai,'\nMenor:',men)
