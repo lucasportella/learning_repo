1cbdcea4eaaa66afec0d3c8541b4c06a58caac40
lucasportella <lucasportellaagu@gmail.com>
2020-05-22 13:30:51 -0300

    ex 70

---
+++ python-codes/curso_em_video_estruturas_de_controle/ex070.1.py
@@ -0,0 +1,24 @@
+s = maisdemil = maisbarato = cont = 0
+maisbaratonome = ' '
+while True:
+    produto = str(input('Nome do produto: ')).strip().upper()
+    preço = float(input('Preço: R$'))
+    cont += 1
+    s += preço
+    if preço > 1000:
+        maisdemil += 1
+    if cont == 1:
+        maisbarato = preço
+        maisbaratonome = produto
+    elif preço < maisbarato:
+        maisbarato = preço
+        maisbaratonome = produto
+    continuar = ' '
+    while continuar not in 'NNÃONAOSSIM':
+        continuar = str(input('Continuar? [S/N] ')).strip().upper()
+    if continuar in 'NNÃONAO':
+        break
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'O total da compra foi R${s:.2f}')
+print(f'Temos {maisdemil} produto(s) custando mais de R$ 1000.00')
+print(f'O produto mais barato foi {maisbaratonome.capitalize()} que custa R${maisbarato:.2f}')
