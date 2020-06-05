be459a71c4c497ddae9278667cc3e310378539af
lucasportella <lucasportellaagu@gmail.com>
2020-06-05 12:28:30 -0300

    ex075 pronto

--- python-codes/m3_curso_em_video_estruturas_compostas/ex075.2.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex075.2.py
@@ -1,17 +1,18 @@
 val = ()
-valpar = ()
+valpar = 0
+valparcum = ()
 for c in range(0,4):
     val += (int(input('Digite um número: ')),)
+    valpar = val[c]
+    if valpar % 2 == 0:
+        valparcum += valpar,
 print(f'\nVocê digitou os valores {val}')
 print(f'O valor 9 apareceu {val.count(9)} vezes.')
 if 3 in val:
     print(f'Posição do primeiro 3 digitado: {val.index(3)+1}')
 else:
     print('Nenhum 3 foi digitado.')
-for c in val:
-    if c % 2 == 0:
-        valpar += c,
-if valpar == ():
+if valparcum == ():
     print('Nenhum número par foi digitado.')
 else:
-    print(f'Os números pares foram: {valpar}')
+    print(f'Os números pares foram: {valparcum}')
