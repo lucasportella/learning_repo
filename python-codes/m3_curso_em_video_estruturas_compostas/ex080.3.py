0dd34f77f24961a656c35837210ddd4906c61f62
lucasportella <lucasportellaagu@gmail.com>
2020-06-22 21:18:26 -0300

    ex 080.3

---
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex080.3.py
@@ -0,0 +1,16 @@
+lista = []
+for c in range(0,5):
+    n = int(input('Digite um valor: '))
+    if c == 0:
+        lista.append(n)
+    elif n > lista[len(lista)-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n >= lista[pos]:
+                pos += 1
+            elif n < lista[pos]:
+                lista.insert(pos,n)
+                break
+print(lista)
