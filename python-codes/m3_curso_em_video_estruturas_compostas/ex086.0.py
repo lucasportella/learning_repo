343c9921305729de40f345aff866ff27e07bb100
lucasportella <lucasportellaagu@gmail.com>
2020-07-05 20:10:29 -0300

    exs 86 second try

---
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex086.0.py
@@ -0,0 +1,10 @@
+matriz = [[0,1,2],
+          [0,1,2],
+          [0,1,2]]
+lista = []
+
+for c in matriz:
+    for d in c:
+        print(f'{c[d], c[0]}')
+        print(f'{c[d], c[1]}')
+        print(f'{c[d], c[2]}')
