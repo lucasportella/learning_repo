0eee591e7a6ff46f6321e155ea0c1762b39ad454
lucasportella <lucasportellaagu@gmail.com>
2020-07-23 19:15:02 -0300

    ex 88 pronto

--- python-codes/m3_curso_em_video_estruturas_compostas/ex088.0.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex088.0.py
@@ -1,11 +1,11 @@
 from time import sleep
-from random import randint
+from random import sample
 lista = []
 num_jogos = int(input('Quantos jogos você quer que eu sorteie? '))
 print(f'=-=-=- Sorteando {num_jogos} jogos =-=-=-')
 for c in range(num_jogos):
-    for d in range(6):
-        lista.append(randint(0,60))
+    lista = sample(range(60), 6)
+    lista.sort()
     sleep(1)
     print(f'Jogo {c+1}: {lista}')
     lista.clear()
