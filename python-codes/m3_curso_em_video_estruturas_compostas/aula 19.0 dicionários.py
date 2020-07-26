1bd8fe70ea3e1f2bfebd1fda87454d2d221647d0
lucasportella <lucasportellaagu@gmail.com>
2020-07-26 20:33:10 -0300

    aula 19 cont

--- python-codes/m3_curso_em_video_estruturas_compostas/aula 19.0 dicionários.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/aula 19.0 dicionários.py
@@ -1,7 +1,31 @@
-dados = {'nome':'Pedro','idade':25}
-print(dados['nome'])
-print(dados['idade'])
-dados['sexo']='M'
-print(dados['sexo'])
-del dados['idade']
-print(dados)
+'''filme = {'título':'Star Wars',
+        'ano':1977,
+        'diretor':'George Lucas'
+        }
+for k, v in filme.items():
+    print(f'O {k} é {v}.')
+
+pessoas ={'nome': 'Gustavo','sexo': 'M', 'idade':22}
+pessoas['nome'] = 'Lucas'
+print(pessoas['nome'])
+
+brasil = []
+estado1 = {'uf': 'Rio de Janerio', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil)
+print(brasil[0]['sigla'])
+print(brasil[1]['uf'])'''
+
+estado = {}
+brasil = []
+for c in range(0,3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+print(brasil)
+print(estado)
+for e in brasil:
+    for k in e.values():
+        print(f'{k}')
