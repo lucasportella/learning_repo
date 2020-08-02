faa83d681dd0cf5fb7066223c0803384dbe387f9
lucasportella <lucasportellaagu@gmail.com>
2020-08-02 18:16:31 -0300

    ex 93.0 guanabara final

--- python-codes/m3_curso_em_video_estruturas_compostas/ex093.0.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex093.0.py
@@ -1,8 +1,8 @@
 dicio = {}
 dicio["Nome"] = str(input('Nome do jogador: '))
-dicio["Partidas"] = int(input(f'Quantas partidas {dicio["Nome"]} jogou? '))
+partidas = int(input(f'Quantas partidas {dicio["Nome"]} jogou? '))
 lista = []
-for c in range(dicio["Partidas"]):
+for c in range(partidas):
     dicio["Gols"] = int((input(f'Quantos gols na partida {c+1}: ')))
     lista.append(dicio["Gols"])
 dicio["Gols"] = lista
@@ -14,10 +14,9 @@ print('-='*30)
 print(dicio)
 print('-='*30)
 for c, v in dicio.items():
-    if c != 'Partidas':
-        print(f'O campo {c} tem o valor {v}')
+    print(f'O campo {c} tem o valor {v}')
 print('-='*30)
-print(f'O jogador {dicio["Nome"]} jogou {dicio["Partidas"]} partidas.')
+print(f'O jogador {dicio["Nome"]} jogou {partidas} partidas.')
 for c, v in enumerate(lista):
     print(f'   => Na partida {c+1}, fez {v} gols.')
 print(f'Foi um total de {total} gols.')
