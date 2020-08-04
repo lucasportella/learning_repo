f3c228bb76424b948faf3bf0efd1acbeccedc379
lucasportella <lucasportellaagu@gmail.com>
2020-08-04 20:33:34 -0300

    94 meio

--- python-codes/m3_curso_em_video_estruturas_compostas/ex094.0.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex094.0.py
@@ -1,16 +1,21 @@
 lista = []
 dicio = {}
+dicio["Nome"] = ""
 dicio["Sexo"] = ""
+dicio["Idade"] = ""
 continuar = ' '
+
 while True:
     dicio["Nome"] = (input('Nome: '))
     lista.append(dicio["Nome"])
     while True:
-        if dicio["Sexo"] != 'M' or 'F':
-            dicio["Sexo"] = str(input('Sexo: [M/F] ')).upper().strip()
+        dicio["Sexo"] = str(input('Sexo: [M/F] ')).upper().strip()
         if dicio["Sexo"] == 'M' or dicio["Sexo"] == 'F':
             lista.append(dicio["Sexo"])
             break
+        if dicio["Sexo"] not in 'M' or 'F':
+            print('Erro! Por favor, digite apenas M ou F')
+
 
 
     dicio["Idade"] = int(input('Idade: '))
@@ -19,7 +24,13 @@ while True:
         continuar = str(input('Quer continuar? [S/N] ')).upper().strip()
         if continuar in 'N':
             break
-        if continuar in 'S':
+        elif continuar in 'S':
             break
+        else:
+            print('Erro! Responda apenas S ou N.')
     if continuar in 'N':
         break
+print('-='*30)
+print(lista)
+print(dicio)
+print(f'Ao todo temos {len(dicio["Nome"])} pessoas cadastradas.')
