434617d31e2e7e14ce6cba3201ca38d2ab0bdfb2
lucasportella <lucasportellaagu@gmail.com>
2020-08-15 21:25:12 -0300

    95 pronto

--- python-codes/m3_curso_em_video_estruturas_compostas/ex095.0.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex095.0.py
@@ -23,15 +23,20 @@ while True:
             break
     if continuar in 'N':
         break
+
 print('-='*30)
 print(f'{"cod":<4}{"nome":<15}{"gols":<20}total')
 print('-'*50)
 for c in range(0,len(geral)):
     print(f'{(c+1):>3} {geral[c]["Nome"]:<15}{str(geral[c]["Gols"]):<20}{geral[c]["Total"]}')
 print('-'*50)
-while True:
-    escolha = int(input('Mostrar dados de qual jogador? (999 para parar) '))
-    if (escolha - 1) > len(geral) or (escolha - 1) < 0:
-        escolha = int(input('Mostrar dados de qual jogador? (999 para parar) '))
-    if (escolha -1) < len(geral) and (escolha -1) > 0:
-        print(f'-- LEVANTAMENTO DO JOGADOR {geral[escolha - 1]["Nome"]}')
+
+escolha = int()
+while escolha != 999:
+    escolha = int(input('Mostar dados de qual jogador? (999 para parar) '))
+    if (escolha - 1) < len(geral) and (escolha - 1) >= 0:
+        print(f'-- LEVANTAMENTO DO JOGADOR {geral[escolha - 1]["Nome"]}:')
+        for c, k in enumerate(geral[escolha - 1]["Gols"]):
+            print(f'No jogo {c+1} fez {k} gol(s).')
+        print('-'*50)
+
