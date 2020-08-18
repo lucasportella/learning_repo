a00bbb68823ed518cfacafa0ae981eb21ba98571
lucasportella <lucasportellaagu@gmail.com>
2020-08-18 12:04:06 -0300

    97 pronto

---
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex097.0.py
@@ -0,0 +1,10 @@
+def escreva(parâmetro):
+    print('~~',end='')
+    print('~' * len(parâmetro))
+    print(f' {parâmetro}')
+    print('~~',end='')
+    print('~' * len(parâmetro))
+
+escreva(f'{"Lucas Portella"}')
+escreva(f'{"Curso de Python no Youtube"}')
+escreva(f'{"CeV"}')
