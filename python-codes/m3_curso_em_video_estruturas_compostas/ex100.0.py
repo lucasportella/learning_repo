fefd043bbe60ba2f6b261a4f8d61af99d34c0a05
lucasportella <lucasportellaagu@gmail.com>
2020-08-19 18:38:51 -0300

    ex 100

---
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex100.0.py
@@ -0,0 +1,21 @@
+from random import randint
+from time import sleep
+numeros = []
+
+def sorteia():
+    print('Sorteando 5 valores da lista:',end=' ')
+    for c in range(5):
+        sleep(0.3)
+        numeros.append(randint(0,10))
+        print(numeros[c],end=' ')
+    print('PRONTO!')
+
+def soma():
+    paressoma = int()
+    for d in numeros:
+        if d % 2 == 0:
+            paressoma += d
+    print(f'Somando os valores pares de {numeros}, temos {paressoma}')
+
+sorteia()
+soma()
