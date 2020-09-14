287f97e525736daf85313588dba0ecb227c692a0
lucasportella <lucasportellaagu@gmail.com>
2020-09-14 10:57:16 -0300

    ex 101 literal value

--- python-codes/m3_curso_em_video_estruturas_compostas/ex101.0.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex101.0.py
@@ -1,18 +1,17 @@
 from datetime import date
 
 def voto(ano_nascimento):
-    ano_nascimento = date.today().year - ano_nascimento
-    if ano_nascimento < 16:
-        return "NÃO VOTA"
-    elif ano_nascimento >= 16 and ano_nascimento < 18:
-        return "VOTO OPCIONAL"
-    elif ano_nascimento >= 18 and ano_nascimento < 70:
-        return "VOTO OBRIGATÓRIO"
-    elif ano_nascimento >= 70:
-        return "VOTO OPCIONAL"
+    idade = date.today().year - ano_nascimento
+    if idade < 16:
+        return print(f"Com {idade} anos: VOTO NEGADO")
+    elif idade >= 16 and idade < 18:
+        return print(f"Com {idade} anos: VOTO OPCIONAL")
+    elif idade >= 18 and idade < 70:
+        return print(f"Com {idade} anos: VOTO OBRIGATÓRIO")
+    elif idade >= 70:
+        return print(f"Com {idade} anos: VOTO OPCIONAL")
 
 
 print('--'*10)
-nasc = int(input("Em que ano você nasceu? "))
-idade = date.today().year - nasc
-print(f"Com {idade} anos: {voto(nasc)}")
+voto(int(input("Em que ano você nasceu? ")))
+
