46aaa1d389504838d7b8833c5a771cb5039c7983
lucasportella <lucasportellaagu@gmail.com>
2020-09-15 21:30:57 -0300

    ex 103 pronto

---
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex103.1.py
@@ -0,0 +1,12 @@
+def ficha(jogador="<desconhecido>",gols=0):
+    print(f'O jogador {jogador} fez {gols} gol(s) no campeonato.')
+n = str(input("nome do Jogador: "))
+g = str(input("Número de Gols: "))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n,g)
