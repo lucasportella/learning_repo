68ef2dd7b9aafefedc2572a18350a57c49a10e83
lucasportella <lucasportellaagu@gmail.com>
2020-09-21 15:20:26 -0300

    ex103.0 done

--- python-codes/m3_curso_em_video_estruturas_compostas/ex106.0.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/ex106.0.py
@@ -1,16 +1,27 @@
-from time import sleep
-def ajuda(a=0):
-    print('\033[0;30;42m~'*30)
-    print('SISTEMA DE AJUDA PyHELP')
-    print('~'*30)
+def ajuda():
+    while True:
+        print('\033[0;30;42m~' * 30)
+        print('SISTEMA DE AJUDA PyHELP')
+        print('~' * 30)
+        print('\033[m', end='')
 
-    a = input('\033[mFunção ou Biblioteca: ')
+        escolha = input('Função ou biblioteca: ')
+        if escolha == "fim":
+            print('\033[0;30;41m~' * 30)
+            print('FIM')
+            print('~' * 30)
+            print('\033[m')
+            break
 
-    print('\033[0;30;44m~'*30)
-    print(f"Acessando o manual do comando '{a}'")
-    print('~'*30)
-    print('\033[m')
-    sleep(1)
+        print('\033[0;30;44m~' * 30)
+        print(f"Acessando o manual do comando '{escolha}'")
+        print('~' * 30)
+        print('\033[m', end='')
+        from time import sleep
+        sleep(1)
+        print('\033[7;30m')
+        print(help(escolha))
+        print('\033[m', end='')
 
 
-print(f'\033[7;30m{ajuda()}')
+print(ajuda())
