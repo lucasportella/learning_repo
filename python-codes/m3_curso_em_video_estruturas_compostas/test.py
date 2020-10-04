f09fffa4bb60e5b96683419b502c22a5ef6d3797
lucasportella <lucasportellaagu@gmail.com>
2020-10-04 16:27:26 -0300

    skipping git add test

--- python-codes/m3_curso_em_video_estruturas_compostas/test.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/test.py
@@ -2,7 +2,7 @@ numero = "the lord of the rings"
 def timesALetterAppears(phrase, letter):
     # Write your code here
     contador = 0
-    for c in range(0giqq,len(phrase),1):
+    for c in range(0,len(phrase),1):
         print(c)
         if phrase[c] == letter:
             contador += 1
