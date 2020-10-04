e1d4d9438cf9e9a7b82dca9cd1078abce0d784fc
lucasportella <lucasportellaagu@gmail.com>
2020-10-04 16:07:10 -0300

    test

--- python-codes/m3_curso_em_video_estruturas_compostas/test.py
+++ python-codes/m3_curso_em_video_estruturas_compostas/test.py
@@ -1,1 +1,11 @@
-teste = "aaa"
+numero = "the lord of the rings"
+def timesALetterAppears(phrase, letter):
+    # Write your code here
+    contador = 0
+    for c in range(0giqq,len(phrase),1):
+        print(c)
+        if phrase[c] == letter:
+            contador += 1
+    return contador
+
+
